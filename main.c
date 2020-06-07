b64222d8cb81fc475af56adf4664cc7f86d5e993
Andrea Franchini <hello@andreafranchini.com>
2020-06-07 16:02:43 +0200

    working without undo/redo

--- main.c
+++ main.c
@@ -3,35 +3,46 @@
 #include <string.h>
 
 #define MAX_INPUT 1024 + 1
-#define CMD_QUIT "q"
 
 // Prototypes
-void parseInput();
+void parseCmd();
+
 void parseSingleCmd();
+
 void parseDoubleCmd();
+
+void change();
+
+void delete();
+
+void print();
+
+void undo();
+
+void redo();
 
 // Data structures
 typedef enum command_type_ {
-	CHANGE,
-	DELETE,
-	PRINT
+    CHANGE,
+    DELETE,
+    PRINT
 } CmdType;
 
 // A list element representing a row of text
 // To be used in a single-linked list
 typedef struct line_ {
-	char text[MAX_INPUT];
-	Line* next;
+    char *text;
+    struct line_ *next;
 } Line;
 
 // A list element representing a command
 // To be used in a double-linked list
-typedef struct command_ {
-	CmdType type;
-	int addr1;
-	int addr2;
-	Cmd* prev;
-	Cmd* next;
+typedef struct cmd_ {
+    CmdType type;
+    int addr1;
+    int addr2;
+    struct cmd_ *prev;
+    struct cmd_ *next;
 } Cmd;
 
 // Global Variables
@@ -45,73 +56,223 @@ int isCmd = 1;
 int addr1, addr2, times;
 
 // the file buffer
-Line* buffer = NULL;
+Line *buffer = NULL;
+
+// State of the editor
+Line *currLine = NULL;
+Line *prevLine = NULL;
+int currLineIndex = 1;
+int totalLines = 1;
+
 
 // the queues to perform undo/redo
-Cmd* undoQueue = NULL;
-Cmd* redoQueue = NULL;
+Cmd *undoStack = NULL;
+Cmd *redoStack = NULL;
+
+// List: Lines of text
+Line *createLine(char *text) {
+    Line *newLine = malloc(sizeof(Line));
+    char *newText = malloc(sizeof(char) * MAX_INPUT);
+    if (newLine && newText) {
+        strcpy(newText, text);
+        newLine->next = NULL;
+        newLine->text = newText;
+        return newLine;
+    }
+    // Print error
+    printf("ERROR: Can't allocate a new Line!");
+    exit(12);
+}
+
+void updateLine(Line *curr, Line *prev, char *text) {
+    Line *newLine;
+    // Create a new list
+    if (prev == NULL && curr == NULL) {
+        newLine = createLine(text);
+        buffer = newLine;
+        totalLines = 1;
+        // Update prev line
+        prevLine = newLine;
+        currLine = NULL;
+        return;
+    }
+    // Append an element to the list
+    if (prev != NULL && curr == NULL) {
+        newLine = createLine(text);
+        prev->next = newLine;
+        totalLines += 1;
+        // Update prev line
+        prevLine = newLine;
+        currLine = NULL;
+        return;
+    }
+    // else...
+    // Replace an element in the list
+    strcpy(curr->text, inputStr);
+    // Update prev line
+    prevLine = curr;
+    currLine = curr->next;
+}
+
+void deleteLine(Line *prev, Line *line) {
+    // delete a line only if it exists
+    if (line) {
+        Line *next = line->next;
+        // Free memory
+        free(line->text);
+        free(line);
+        if (prev) {
+            // it wasn't the first element of the list
+            // link together the prev and the next element
+            prev->next = next;
+            currLine = next;
+            prevLine = prev;
+        } else {
+            // it was the first element of the list
+            buffer = next;
+            currLine = next;
+            prevLine = NULL;
+        }
+        totalLines -= 1;
+    }
+}
+
+void printLine(Line *line) {
+    if (line)
+        printf("%s", line->text);
+    else
+        printf(".");
+}
+
+// Stack: undo/redo
 
 // Entrypoint
 int main() {
-	do {
-		fgets(inputStr, MAX_INPUT, stdin);
-		parseInput();
-	} while(1);
-	
-	return 0;
+    do {
+        fgets(inputStr, MAX_INPUT, stdin);
+        parseCmd();
+    } while (1);
+
+    return 0;
 }
 
-void parseInput() {
-	// I need to parse the command
-	if(isCmd) {
-		// Get the lenght of the input
-		size_t len = strlen(inputStr) - 1;
-		// Get command char (the identifier)
-		char cmd = inputStr[len - 1];
-		// "Delete" the cmd char
-		inputStr[len - 1] = '\0';
-		// Call the right parser for each command
-		switch (cmd) {
-			case 'c': // Change
-				parseDoubleCmd();
-				isCmd = 0;
-				break;
-			case 'd': // Delete
-				parseDoubleCmd();
-				isCmd = 1;
-				break;
-			case 'p': // Print
-				parseDoubleCmd();
-				isCmd = 1;
-				break;
-			case 'u': // Undo
-				parseSingleCmd();
-				break;
-			case 'r': // Redo
-				parseSingleCmd();
-				break;
-			case 'q':
-				exit(0);
-				return;
-			default:
-				break;
-		}
-	} else {
-		// I just need to handle the content
-	}
+void parseCmd() {
+    // I need to parse the command
+
+    // Get the lenght of the input
+    size_t len = strlen(inputStr) - 1;
+    // Get command char (the identifier)
+    char cmd = inputStr[len - 1];
+    // "Delete" the cmd char
+    inputStr[len - 1] = '\0';
+    // Call the right parser for each command
+    switch (cmd) {
+        case 'c': // Change
+            parseDoubleCmd();
+            isCmd = 0;
+            change();
+            break;
+        case 'd': // Delete
+            parseDoubleCmd();
+            isCmd = 1;
+            delete();
+            break;
+        case 'p': // Print
+            parseDoubleCmd();
+            isCmd = 1;
+            print();
+            break;
+        case 'u': // Undo
+            parseSingleCmd();
+            undo();
+            break;
+        case 'r': // Redo
+            parseSingleCmd();
+            redo();
+            break;
+        case 'q':
+            exit(0);
+            return;
+        default:
+            break;
+    }
 }
 
 // If you're getting weird values and need fixes, check 
 // https://stackoverflow.com/questions/5493235
 void parseSingleCmd() {
-	times = strtol(inputStr, NULL, 10);
-	printf("> %d\n", times);
+    times = strtol(inputStr, NULL, 10);
 }
 
 void parseDoubleCmd() {
-	char* token1 = strtok(inputStr, ",");
-	char* token2 = strtok(NULL, ",");
-	addr1 = strtol(token1, NULL, 10);
-	addr2 = strtol(token2, NULL, 10);
-	printf("> %d, %d\n", addr1, addr2);
+    char *token1 = strtok(inputStr, ",");
+    char *token2 = strtok(NULL, ",");
+    addr1 = strtol(token1, NULL, 10);
+    addr2 = strtol(token2, NULL, 10);
+}
+
+void change() {
+    if (addr1 > 0 && addr1 <= totalLines + 1) {
+        // Start from the beginning (line 1)
+        currLine = buffer;
+        prevLine = NULL;
+        // Move to addr1 line
+        for (currLineIndex = 1; currLineIndex < addr1; currLineIndex++) {
+            prevLine = currLine;
+            currLine = currLine->next;
+        }
+        // Change each line till addr2 line
+        for (int i = addr1; i <= addr2; i += 1) {
+            // save input text in inputStr
+            fgets(inputStr, MAX_INPUT, stdin);
+            // updateLine also updates prevLine and currLine
+            updateLine(currLine, prevLine, inputStr);
+        }
+        // Eat the final '.'
+        fgets(inputStr, MAX_INPUT, stdin);
+    }
+}
+
+void delete() {
+    if (addr1 > 0 && addr1 <= totalLines + 1) {
+        // Start from the beginning (line 1)
+        currLine = buffer;
+        prevLine = NULL;
+        // Move to addr1 line
+        for (currLineIndex = 1; currLineIndex < addr1; currLineIndex++) {
+            prevLine = currLine;
+            currLine = currLine->next;
+        }
+        // Delete each line till addr2 line
+        for (int i = addr1; i <= addr2; i += 1) {
+            deleteLine(prevLine, currLine);
+        }
+    }
+}
+
+void print() {
+    // Start from the beginning
+    currLine = buffer;
+
+    // Move to addr1 line
+    for (currLineIndex = 1; currLineIndex < addr1; currLineIndex++) {
+        currLine = currLine->next;
+    }
+    // Print each line till addr2 line
+    for (int i = addr1; i <= addr2; i += 1) {
+        if (currLine != NULL) {
+            printLine(currLine);
+            currLine = currLine->next;
+        } else {
+            printf(".\n");
+        }
+    }
+}
+
+void undo() {
+
+}
+
+void redo() {
+
 }
